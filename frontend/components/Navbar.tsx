1535824cfab0390034acc739d781e7cb16d1c3ab
Dev More <devmore.2004@gmail.com>
2025-09-22 01:56:54 +0530

    landing pagee changes

---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import React from "react";
+import { ThemeToggleButton2 } from "./theme-button";
+import { ThemeToggleButton, useThemeToggle } from "./skiper";
+import StaggeredMenu from "./StaggeredMenu";
+
+export default function Navbar() {
+  return <div className="flex justify-end items-center py-4"></div>;
+}
